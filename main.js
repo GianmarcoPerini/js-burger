167bd9611f226d02c5ed5bf389a654d11fd5b8a5
GianmarcoPerini <g.marco.perini@gmail.com>
2021-08-30 15:59:43 +0200

    init3

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
 
 let check = document.getElementsByClassName("box");
+let calcolo = document.querySelector("button")
+let ingredienti = []
+let somma = 0
 
+UncheckAll()
 
-
-
-document.addEventListener("click", function(){
-
-    for(i = 0; i < check.length; i++){
-        if(check[i].checked == true){
-            console.log(check.value);
+for(let i = 0; i < check.length; i++){
+    let checkBox = check[i]
+    checkBox.addEventListener("click", function(){
+        if(checkBox.checked == true){
+            let num = parseInt(checkBox.value)
+            ingredienti.push(num)
+            console.log(ingredienti)
+        }else{
+            ingredienti.pop()
         }
+    })
+}
+
+calcolo.addEventListener("click", function(){
+    for(let x = 0; x < ingredienti.length; x++){
+        somma += ingredienti[x]
     }
+    console.log(somma);
 
 })
+
+
+
+
+
+
+
+function UncheckAll(){ 
+    var w = document.getElementsByClassName('box'); 
+    for(var i = 0; i < w.length; i++){ 
+        if(w[i].type=='checkbox'){ 
+        w[i].checked = false; 
+        }
+    }
+} 
