7476735f1924dd72fb3c95fe0ee40a81b9029798
GianmarcoPerini <g.marco.perini@gmail.com>
2021-08-31 03:04:41 +0200

    refactoring_2

--- main.js
+++ main.js
@@ -1,44 +1,49 @@
-let check = document.getElementsByClassName("box");
-let bottone = document.getElementById("btn")
-let scontoCoupon = ["SUPERSCONTO2021","CICCIOELONTO","PANINATOCUP21"]
-let ingredientiPrezzo = []
-let somma = 0
+let value = document.getElementsByClassName('box');
+let btn = document.querySelector('button');
+let txt = document.getElementById('codici');
+let listinoPrezzi = [];
+let somma;
+let prezzoPanino = 5;
+let prezzoTotale;
+let prezzoScontato;
+let coupon = ["SUPERSCONTO2021","CICCIOELONTO","PANINATOCUP21"];
 
 UncheckAll()
+btn.addEventListener('click', function(){
+    somma = 0;
+    for(let x = 0; x < listinoPrezzi.length; x++){
+        somma += listinoPrezzi[x] 
+    }
+    prezzoTotale = somma + prezzoPanino;
+    prezzoScontato = prezzoTotale * 0.85
+    if(coupon.includes(txt.value)){
+        document.getElementById("prezzo-finale").innerHTML = prezzoScontato.toFixed(2)
+    } else{
+        document.getElementById("prezzo-finale").innerHTML = prezzoTotale.toFixed(2)
+    }
 
-for(let i = 0; i < check.length; i++){
-    let checkBox = check[i]
-    checkBox.addEventListener("click", function(){
-        if(checkBox.checked == true){
-            let num = parseFloat(checkBox.value)
-            ingredientiPrezzo.push(num)
-            console.log(ingredientiPrezzo)
-        }else{
-            ingredientiPrezzo.pop()
+    UncheckAll()
+    if( listinoPrezzi.length){
+        listinoPrezzi.length = 0
+    }
+})
+
+for(let i = 0; i < value.length; i++){
+    let takeValue = value[i]; 
+    takeValue.addEventListener("click", function(){
+        if(takeValue.checked == true){
+            listinoPrezzi.push(parseFloat(takeValue.value))
+            console.log(listinoPrezzi);
         }
     })
 }
 
-bottone.addEventListener("click", function(){
-    let sconti = document.getElementById("cupon").value;
-    let prezzoPieno = somma + 5
-    let prezzoScontato = (somma + 5) * 0.8
 
-
-    for(let x = 0; x < ingredientiPrezzo.length; x++){
-        somma += ingredientiPrezzo[x]
-    }
-    if(scontoCoupon.includes(sconti)){
-        document.getElementById("prezzo-finale").innerHTML = prezzoScontato.toFixed(2)
-    } else{
-        document.getElementById("prezzo-finale").innerHTML = prezzoPieno.toFixed(2)
-    }
-})
 
 function UncheckAll(){  
-    for(let i = 0; i < check.length; i++){ 
-        if(check[i].type == 'checkbox'){ 
-        check[i].checked = false; 
+    for(let i = 0; i < value.length; i++){ 
+        if(value[i].type == 'checkbox'){ 
+        value[i].checked = false; 
         }
     }
 } 
